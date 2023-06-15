d328c973e13acefa45b52d9603ec34eb10e39af8
Alexandra Mampouya <mampouyaalexandra3@gmail.com>
2023-06-15 23:39:06 +0100

    Six commit

--- SOUTENANCE_MAVRE/SOUTENANCE_MAVRE/urls.py
+++ SOUTENANCE_MAVRE/SOUTENANCE_MAVRE/urls.py
@@ -21,5 +21,6 @@ from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include('utilisateurs.urls')),
     path('api/', include('mobilemavre.urls')),
 ]
